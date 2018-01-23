8c8d511cf38567397d919f4f6d8484b39ec7b94f
nauqnew <dozenthree@gmail.com>
2018-01-23 17:11:50 +0800

    multiasset test

--- main.go
+++ main.go
@@ -5,20 +5,19 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	//"strconv"
+	"encoding/json"
+	"github.com/bitly/go-simplejson"
 	"time"
 )
 
 func main() {
 	start := time.Now()
-	for i := 0; i < 1000; i++ {
-		postRequest()
-	}
+	//postMultiAsset()
 	elapsed := time.Since(start)
 	fmt.Println("Time elapsed after 30 transactions", elapsed)
 }
 
-func postRequest() {
+func postRequest() error {
 
 	url := "http://172.25.62.160:8080/bs_platform/loan/assetUpload"
 
@@ -31,7 +30,7 @@ func postRequest() {
 
 	//post:= "{\"assetUid\":\"160815609421112015\",\"timestamp\":\"123456\",\"assetDetails\":\"160815609421112015,***n66_m,2016-08-15 20:41:30,2017-08-15 23:59:59,12,2399,199.92,0,24,0,3,3,1,1,HT201606300001\",\"orgCode\":\"jyzb\"}"
 
-	post:= "{\"assetUid\":\"160815609421112015\",\"timestamp\":\"123456\",\"bizContent\":{\"assetDetails\":\"160815609421112015,***n66_m,2016-08-15 20:41:30,2017-08-15 23:59:59,12,2399,199.92,0,24,0,3,3,1,1,HT201606300001\"},\"orgCode\":\"jyzb\",\"outTradeNo\":\"HT201606300001\"}"
+	post := "{\"assetUid\":\"160815609421112018\",\"timestamp\":\"123456\",\"bizContent\":{\"assetDetails\":\"160815609421112015,***n66_m,2016-08-15 20:41:30,2017-08-15 23:59:59,12,2399,199.92,0,24,0,3,3,1,1,HT201606300001\"},\"orgCode\":\"jyzb\",\"outTradeNo\":\"HT201606300001\"}"
 	var jsonStr = []byte(post)
 	//fmt.Println("jsonStr", jsonStr)
 	//fmt.Println("new_str", bytes.NewBuffer(jsonStr))
@@ -50,5 +49,75 @@ func postRequest() {
 	//fmt.Println("response Status:", resp.Status)
 	body, _ := ioutil.ReadAll(resp.Body)
 	fmt.Println("response Body:", string(body))
+	return nil
 
+}
+
+var bizContent = `{
+        "assets":[
+            {
+                "assetUid":"160815609421112015",
+                "assetDetails":"160815609421112015,***n66_m,2016-08-15 20:41:30,2017-08-15 23:59:59,12,2399,199.92,0,24,0,3,3,1,1,HT201606300001"
+            }
+        ]
+    }`
+
+var asset = `{        
+                "assetUid":"160815609421112017",
+                "assetDetails":"160815609421112017,***n66_m,2016-08-15 20:41:30,2017-08-15 23:59:59,12,2399,199.92,0,24,0,3,3,1,1,HT201606300001"
+}
+`
+
+var (
+	batchSize = 100
+)
+
+func makeBatch() (string, error) {
+	var arr []interface{}
+
+	js, _ := simplejson.NewJson([]byte(bizContent))
+	nodes, _ := js.Map()
+	p := nodes["assets"]
+	d := p.([]interface{})
+	for _, v := range d {
+		for i := 0; i < batchSize; i++ {
+			arr = append(arr, v)
+		}
+	}
+
+	res := make(map[string]interface{})
+	res["assets"] = arr
+	c, _ := json.Marshal(res)
+	//fmt.Println(string(c))
+	res["bizContent"] = string(c)
+	res["assetUid"] = "160815609421112015"
+	res["timestamp"] = "20180123155136"
+	res["outTradeNo"] = "160815609421112015"
+	res["orgCode"] = "jyzb"
+	delete(res, "assets")
+	request, err := json.Marshal(res)
+	return string(request), err
+}
+
+func postMultiAsset(request string ) error {
+
+	url := "http://172.25.62.160:8080/bs_platform/loan/assetUpload"
+	var jsonStr = []byte(request)
+	//fmt.Println("jsonStr", jsonStr)
+	//fmt.Println("new_str", bytes.NewBuffer(jsonStr))
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	//fmt.Println("response Status:", resp.Status)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
+	return nil
 }
--- main_test.go
+++ main_test.go
@@ -8,14 +8,18 @@ import (
 )
 
 func TestSerial(t *testing.T) {
+
 	start := time.Now()
-	for i := 0; i < 10; i++ {
-		postRequest()
+	request, _ := makeBatch()
+	for i := 0; i < 100; i++ {
+		//postRequest()
+		postMultiAsset(request)
 	}
 	elapsed := time.Since(start)
 	fmt.Println("Time elapsed ", elapsed)
 }
 
+// 每一个post请求一个goroutine显然不合适！！！！！！！
 func TestParallel(t *testing.T) {
 	var wg sync.WaitGroup
 	start := time.Now()
@@ -27,6 +31,48 @@ func TestParallel(t *testing.T) {
 		}()
 	}
 	wg.Wait()
+	elapsed := time.Since(start)
+	fmt.Println("Time elapsed ", elapsed)
+
+}
+
+//配置任务数和协程数量
+var (
+	numberOfWorkers = 3
+	totalJobs       = 100
+)
+
+//使用channel实现线程池功能
+func TestPool(t *testing.T) {
+	request, _ := makeBatch()
+	start := time.Now()
+	pool := new(GoroutinePool)
+	pool.Init(numberOfWorkers, totalJobs)
+
+	for i := 0; i < totalJobs; i++ {
+		pool.AddTask(func() error {
+			//return postRequest()
+			return postMultiAsset(request)
+		})
+	}
+
+	isFinished := false
+
+	pool.SetFinishCallback(func() {
+		func(isFinished *bool) {
+			*isFinished = true
+		}(&isFinished)
+
+	})
+
+	pool.Start()
+
+	for !isFinished {
+		time.Sleep(time.Millisecond * 100)
+	}
+
+	pool.Stop()
+	fmt.Println("All is well !")
 	elapsed := time.Since(start)
 	fmt.Println("Time elapsed ", elapsed)
 
